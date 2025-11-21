6444f94f88c96dadc9bbd48a72a5677865532ab2
Romanenko Vladimir <bonyshd@gmail.com>
2025-11-21 11:53:31 +0300

    Урок 3

--- bot/handlers/start_handler.py
+++ bot/handlers/start_handler.py
@@ -6,9 +6,18 @@ from telegram import (
 )
 from telegram.ext import ContextTypes
 from config.config import WEBHOOK_URL, WEBAPP_PATH
+from db.users_crud import create_user, get_user_by_telegram_id, update_user_username, delete_user
+from logs.logger import logger
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user = await get_user_by_telegram_id(update.effective_user.id)
+    if not user:
+        user = await create_user(update.effective_user.id, update.effective_user.username)
+    logger.info(f"Пользователь {user.id} ({user.username}) ({user.telegram_id}) ({user.created_at}) начал диалог")
+    user = await update_user_username(user.telegram_id, 'биба')
+    logger.info(f"Пользователь {user.id} ({user.username}) ({user.telegram_id}) ({user.created_at}) начал диалог")
+    await delete_user(user.telegram_id)
     keyboard = [
         [
             InlineKeyboardButton(
---
+++ db/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncSession,
+)
+from sqlalchemy.orm import sessionmaker
+from typing import AsyncGenerator
+from db.models import Base
+from contextlib import asynccontextmanager
+
+
+engine = create_async_engine("sqlite+aiosqlite:///database.db", echo=True)
+
+async_session_maker = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
+
+@asynccontextmanager
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+async def init_db() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all) # Создавать все таблицы в базе данных
